0494a4aa0cf1f87018e73d60b15fcd88bab30a1e
hilawnabangus <jaymaryugo90@gmail.com>
2026-09-14 18:01:39 +0800

    update for todos

--- demo/myapp/views.py
+++ demo/myapp/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render, HttpResponse
+from .models import TodoItem
+
 
 # Create your views here.
 def home(request):
-    return HttpResponse("hello world!")
+    return render(request=request, template_name="home.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request=request, template_name="todos.html", context={"todos": items})
